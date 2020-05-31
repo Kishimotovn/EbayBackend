83e27f5993bf3d75c6667b31d5548834bc047dce
Phan Tran <Phan.Tran@skybettingandgaming.com>
2020-05-31 17:37:04 +0100

    Added configuration for DatabaseURL

--- Sources/App/Setup/databases.swift
+++ Sources/App/Setup/databases.swift
@@ -11,10 +11,14 @@ import Fluent
 import FluentPostgresDriver
 
 public func databases(app: Application) throws {
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+    if let databaseURL = Environment.process.DATABASE_URL {
+        try app.databases.use(.postgres(url: databaseURL), as: .psql)
+    } else {
+        app.databases.use(.postgres(
+            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+            database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+        ), as: .psql)
+    }
 }
