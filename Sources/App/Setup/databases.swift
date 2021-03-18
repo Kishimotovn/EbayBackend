08b321485c197e793b6a4dbeede2ef092e9a48f3
Phan Tran <Phan.Tran@skybettingandgaming.com>
2021-03-18 23:01:07 +0000

    Hardcode db info

--- Sources/App/Setup/databases.swift
+++ Sources/App/Setup/databases.swift
@@ -11,16 +11,24 @@ import Fluent
 import FluentPostgresDriver
 
 public func databases(app: Application) throws {
-    if let databaseURL = Environment.process.DATABASE_URL {
-        try app.databases.use(.postgres(url: databaseURL), as: .psql)
-    } else {
-        app.databases.use(.postgres(
-            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-            database: Environment.get("DATABASE_NAME") ?? "ebay_db"
-        ), as: .psql)
-    }
+    app.databases.use(.postgres(
+        hostname: "ec2-54-146-4-66.compute-1.amazonaws.com",
+        port: 5432,
+        username: "ymtwkzhndbzcxg",
+        password: "ea570d9b71b2075811e869a8b6d8b0f32613c224768e9500f080af678ca69789",
+        database: "d5acg9sgiqcfkv",
+        tlsConfiguration: TLSConfiguration.forClient(certificateVerification: .none)
+    ), as: .psql)
+//    if let databaseURL = Environment.process.DATABASE_URL {
+//        try app.databases.use(.postgres(url: databaseURL), as: .psql)
+//    } else {
+//        app.databases.use(.postgres(
+//            hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+//            username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
+//            password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
+//            database: Environment.get("DATABASE_NAME") ?? "ebay_db"
+//        ), as: .psql)
+//    }
     app.databases.middleware.use(JobMonitoringMiddleware(), on: .psql)
 }
 
