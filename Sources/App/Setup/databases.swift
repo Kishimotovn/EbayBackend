4458f0114d020dbdb235c9dd3a5242112f99b795
Phan Tran <Phan.Tran@skybettingandgaming.com>
2021-03-18 18:47:13 +0000

    Add ssl query to db url

--- Sources/App/Setup/databases.swift
+++ Sources/App/Setup/databases.swift
@@ -11,8 +11,9 @@ import Fluent
 import FluentPostgresDriver
 
 public func databases(app: Application) throws {
-    if let databaseURL = Environment.process.DATABASE_URL {
-        try app.databases.use(.postgres(url: databaseURL), as: .psql)
+    if let databaseURL = Environment.process.DATABASE_URL, let url = URL(string: databaseURL) {
+        let sslEnabledURL = url.appending("ssl", value: "true")
+        try app.databases.use(.postgres(url: sslEnabledURL), as: .psql)
     } else {
         app.databases.use(.postgres(
             hostname: Environment.get("DATABASE_HOST") ?? "localhost",
@@ -22,4 +23,26 @@ public func databases(app: Application) throws {
         ), as: .psql)
     }
     app.databases.middleware.use(JobMonitoringMiddleware(), on: .psql)
+}
+
+extension URL {
+    func appending(_ queryItem: String, value: String?) -> URL {
+
+        guard var urlComponents = URLComponents(string: absoluteString) else { return absoluteURL }
+
+        // Create array of existing query items
+        var queryItems: [URLQueryItem] = urlComponents.queryItems ??  []
+
+        // Create query item
+        let queryItem = URLQueryItem(name: queryItem, value: value)
+
+        // Append the new query item in the existing query items array
+        queryItems.append(queryItem)
+
+        // Append updated query items array in the url component object
+        urlComponents.queryItems = queryItems
+
+        // Returns the url from new url components
+        return urlComponents.url!
+    }
 }
